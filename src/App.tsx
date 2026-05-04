073c793e8788a5e893a80ca0495521917167c71a
Chris Riddell <chris.riddell@ospri.co.nz>
2026-05-04 16:13:40 +1200

    add networks back in to dev

--- src/App.tsx
+++ src/App.tsx
@@ -655,10 +655,6 @@ export default function App() {
   }, [saveActiveProfile]);
 
   const hasMeeting = meetingPoint !== null;
-<<<<<<< Updated upstream
-  // const canCalculate = hasMeeting && people.length > 0;
-=======
->>>>>>> Stashed changes
   const tileUrl = TILE_URLS[theme];
 
   return (
