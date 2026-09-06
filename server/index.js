95dfb9af6eb1dfe4e834d0caf31b197fd5607ff7
Chris <chris@chrisnaimee.com>
2026-09-06 17:21:08 +1200

    remove debug messages

--- server/index.js
+++ server/index.js
@@ -133,10 +133,6 @@ app.get('/health', async (_, res) => {
 app.get('/config.js', (_, res) => {
   res.set('Cache-Control', 'no-store');
   const hasCartoApiKey = Boolean(process.env.CARTO_API_KEY);
-  console.info('[Carto tiles] Serving runtime config', {
-    hasApiKey: hasCartoApiKey,
-    length: process.env.CARTO_API_KEY?.length || 0,
-  });
   res.type('application/javascript').send(
     `window.__APP_CONFIG__ = ${JSON.stringify({ cartoApiKey: process.env.CARTO_API_KEY || '' })};`
   );
